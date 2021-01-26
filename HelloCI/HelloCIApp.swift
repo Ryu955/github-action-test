e3bbb10428231453e57fbc985f9949107b6aff7c
Ryu955 <ryuta_ishikawa@voyagegroup.com>
2021-01-26 17:34:58 +0900

    Initial Commit

---
+++ HelloCI/HelloCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCIApp.swift
+//  HelloCI
+//
+//  Created by 石河　竜太 on 2021/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
